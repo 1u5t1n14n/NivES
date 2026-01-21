86e34776ac4b130534a58b019596851c059f399c
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2026-01-21 17:33:11 +0100

    Add Paperless, Tika and Gotenberg services for Server

---
+++ system/server/paperless.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, lib, ... }:
+
+let
+	cfg = config.services.paperless;
+
+in
+{
+
+	# TODO: Secret Management
+
+	services = {
+		paperless = {
+			address = "0.0.0.0";
+			port = 28981;
+
+			consumptionDir = "${config.services.paperless.dataDir}/${config.services.paperless.settings.PAPERLESS_APP_TITLE}";
+			consumptionDirIsPublic = true;
+			passwordFile = config.sops.secrets."services/paperless".path;
+
+			settings = {
+				PAPERLESS_APP_TITLE = "Archiv";
+				PAPERLESS_ADMIN_USER = config.services.nextcloud.config.adminuser;
+				PAPERLESS_OCR_LANGUAGE = "deu+eng";
+				PAPERLESS_OCR_USER_ARGS = {
+					optimize = 1;
+					pdfa_image_compression = "lossless";
+				};
+				PAPERLESS_EMPTY_TRASH_DELAY = 15;
+				PAPERLESS_TIKA_ENABLED = true;
+				PAPERLESS_GOTENBERG_ENABLED = true;
+				PAPERLESS_TIKA_ENDPOINT = "http://${config.services.tika.listenAddress}:${toString config.services.tika.port}";
+				PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://${config.services.gotenberg.bindIP}:${toString config.services.gotenberg.port}";
+			};
+		};
+
+		tika = {
+			enable = config.services.paperless.settings.PAPERLESS_TIKA_ENABLED;
+			enableOCR = true;
+
+			port = 9998;
+		};
+		gotenberg = {
+			enable = config.services.paperless.settings.PAPERLESS_GOTENBERG_ENABLED;
+
+			chromium = {
+				package = pkgs.ungoogled-chromium;
+				disableJavascript = true;
+			};
+
+			extraArgs = [ "--chromium-allow-list=file:///tmp/.*" ];
+			extraFontPackages = config.fonts.packages;
+
+			port = 3001;
+		};
+	};
+
+	networking.firewall.allowedTCPPorts = lib.mkIf (config.services.paperless.enable && (config.services.paperless.address == "0.0.0.0"))
+		[ cfg.port ];
+
+}
