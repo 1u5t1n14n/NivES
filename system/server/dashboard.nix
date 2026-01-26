29e09a46ffc6a6a86ab34b750995431cc912f8fe
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2026-01-26 18:17:37 +0100

    Add Resources and Immich to Dashboard

--- system/server/dashboard.nix
+++ system/server/dashboard.nix
@@ -12,6 +12,31 @@
 				toString config.services.homepage-dashboard.listenPort
 			}";
 
+		widgets = [
+			{
+				resources = {
+					cpu = true;
+					disk = "/";
+					memory = true;
+				};
+			}
+		];
+
+		services = [
+			{
+				Sync = [
+					{
+						Immich = {
+							description = "Easily synchronise your Photos";
+							href = "http://192.168.178.185:2283";
+							ping = "http://192.168.178.185:2283";
+							icon = "immich.svg";
+						};
+					}
+				];
+			}
+		];
+
 		openFirewall = true;
 	};
 
