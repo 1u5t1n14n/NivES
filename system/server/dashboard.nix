c2e8dfdd0c95c3d253dcfec72b0943337205651c
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2026-01-26 13:17:54 +0100

    Fix Syntax at Dashboard

--- system/server/dashboard.nix
+++ system/server/dashboard.nix
@@ -1,15 +1,15 @@
-{ ... }:
+{ config, ... }:
 
 {
 
 	services.homepage-dashboard = {
 		listenPort = 8082;
 		allowedHosts = "localhost:${
-				config.services.homepage-dashboard.listenPort
+				toString config.services.homepage-dashboard.listenPort
 			},127.0.0.1:${
-				config.services.homepage-dashboard.listenPort
+				toString config.services.homepage-dashboard.listenPort
 			},192.168.178.185:${
-				config.services.homepage-dashboard.listenPort
+				toString config.services.homepage-dashboard.listenPort
 			}";
 
 		openFirewall = true;
