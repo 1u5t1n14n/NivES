4cad5fd608604d6473c2a2d21b1614c6cb382acb
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2026-01-26 12:44:57 +0100

    Allow IP Access to Dashboard

--- system/server/dashboard.nix
+++ system/server/dashboard.nix
@@ -3,8 +3,14 @@
 {
 
 	services.homepage-dashboard = {
-		port = 8082;
-		allowedHosts = "localhost:8082,127.0.0.1:8082,192.168.178.188:8082";
+		listenPort = 8082;
+		allowedHosts = "localhost:${
+				config.services.homepage-dashboard.listenPort
+			},127.0.0.1:${
+				config.services.homepage-dashboard.listenPort
+			},192.168.178.185:${
+				config.services.homepage-dashboard.listenPort
+			}";
 
 		openFirewall = true;
 	};
