a94b1f99a51bcff2832938d1dc9de8cecf873e35
1u5t1n14n <177038973+1u5t1n14n@users.noreply.github.com>
2026-02-05 14:36:18 +0100

    Add second Activation Script for Testing

--- system/config.nix
+++ system/config.nix
@@ -10,9 +10,17 @@
 	++ lib.optionals (!host.desktop)
 		[ ./server ];
 
+	# Home-Manager
 	environment.systemPackages = [ pkgs.home-manager ];
+
+	# Work-around for continued Systemd Service. Probably
+	# other way to prevent erroring out.
 	system.activationScripts.backupRemove = ''
 		find ${config.users.users.${host.user}.home} -type f -name "*.hmBackup" -delete
 	'';
+
+	# Needed for Home-Manager Usage with Impermanence
+	# Probably easier to do, but who cares
+	system.userActivationScripts.backupRemove = config.system.activationScripts.backupRemove;
 
 }
